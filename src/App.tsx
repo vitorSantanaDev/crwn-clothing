8ea1b4f259ce358c56227b2638702e4afc106479
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-29 16:47:52 -0700

    chore: :seedling: creating home, login and shop route

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,8 @@
 import React from 'react'
-import { SectionCategories } from 'components'
-
-const mockData = [
-  {
-    id: String(1),
-    action: 'Example',
-    title: 'hats',
-    urlImageBackground: 'https://i.ibb.co/cvpntL1/hats.png'
-  },
-  {
-    id: String(2),
-    action: 'Example',
-    title: 'jackets',
-    urlImageBackground: 'https://i.ibb.co/px2tCc3/jackets.png'
-  },
-  {
-    id: String(3),
-    action: 'Example',
-    title: 'sneakers',
-    urlImageBackground: 'https://i.ibb.co/0jqHpnp/sneakers.png'
-  },
-  {
-    id: String(4),
-    action: 'Example',
-    title: 'womens',
-    urlImageBackground: 'https://i.ibb.co/GCCdy8t/womens.png'
-  },
-  {
-    id: String(5),
-    action: 'Example',
-    title: 'mens',
-    urlImageBackground: 'https://i.ibb.co/R70vBrQ/men.png'
-  }
-]
+import RoutesApp from 'routes/index.routes'
 
 function App() {
-  return (
-    <div className="App">
-      <SectionCategories categories={mockData} />
-    </div>
-  )
+  return <RoutesApp />
 }
 
 export default App

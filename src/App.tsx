3e0262bbf27269820bf0cea7e027218c58332a5c
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-07-10 17:55:40 -0700

    refactor: :recycle: adding the contexts in the right file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React, { Fragment } from 'react'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.min.css'
 
 import RoutesApp from 'routes/index.routes'
-import { UserContextProvider } from 'contexts/UserContext'
 
 function App() {
   return (
     <Fragment>
-      <UserContextProvider>
-        <RoutesApp />
-        <ToastContainer />
-      </UserContextProvider>
+      <RoutesApp />
     </Fragment>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { GlobalStyle } from 'styles/global'
+import { ToastContainer } from 'react-toastify'
+
+import { UserContextProvider, ProductsProvider, CartProvider } from 'contexts'
+
 import App from './App'
+import { GlobalStyle } from 'styles/global'
+
+import 'react-toastify/dist/ReactToastify.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <UserContextProvider>
+      <ProductsProvider>
+        <CartProvider>
+          <GlobalStyle />
+          <App />
+          <ToastContainer />
+        </CartProvider>
+      </ProductsProvider>
+    </UserContextProvider>
   </React.StrictMode>
 )

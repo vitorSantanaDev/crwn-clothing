ded7461fbb1b43cf5b2d518e35660000eeaa8dd4
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-07-10 17:54:02 -0700

    feat: :art: creating the Shop page

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,3 +1,18 @@
+import { useContext } from 'react'
+
+import { ProductContext } from 'contexts'
+
+import { ProductCard } from 'components'
+
+import * as S from './styles'
+
 export default function Shop() {
-  return <h1>Shop</h1>
+  const { products } = useContext(ProductContext)
+  return (
+    <S.ShopWrapper>
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </S.ShopWrapper>
+  )
 }
---
+++ src/pages/Shop/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const ShopWrapper = styled.main`
+  display: grid;
+  column-gap: 10px;
+  row-gap: 50px;
+  grid-template-columns: repeat(4, 1fr);
+`

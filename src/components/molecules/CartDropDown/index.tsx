d0d0a6d7d21294f11bf960e1b9e8e669d2ae861d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-07-12 15:54:39 -0700

    feat: :sparkles: creating checkout route

--- src/components/molecules/CartDropDown/index.tsx
+++ src/components/molecules/CartDropDown/index.tsx
@@ -1,14 +1,23 @@
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { CartContext } from 'contexts'
 
 import { ButtonTypeStyleEnum } from 'components/atoms/ButtonComponent/types'
 import { ButtonComponent, CartItem } from 'components/atoms'
 
+import routesName from 'routes/enum.routes'
+
 import * as S from './styles'
 
 export default function CartDropDown() {
-  const { cartItems } = useContext(CartContext)
+  const { cartItems, isCartOpen, setIsCartOpen } = useContext(CartContext)
+  const navigation = useNavigate()
+
+  const handleNavigationToCheckoutPage = () => {
+    setIsCartOpen?.(!isCartOpen)
+    navigation(routesName.CHECKOUT)
+  }
 
   return (
     <S.CartDropDownWrapper>
@@ -17,7 +26,10 @@ export default function CartDropDown() {
           <CartItem key={item.id} {...item} />
         ))}
       </S.CartItems>
-      <ButtonComponent buttonTypeStyle={ButtonTypeStyleEnum.DEFAULT}>
+      <ButtonComponent
+        handleClick={handleNavigationToCheckoutPage}
+        buttonTypeStyle={ButtonTypeStyleEnum.DEFAULT}
+      >
         Pagamento
       </ButtonComponent>
     </S.CartDropDownWrapper>

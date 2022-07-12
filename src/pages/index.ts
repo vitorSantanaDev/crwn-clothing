f0df8e15b557d7f092e864d8cb04d2dcde8adb67
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-07-12 15:53:28 -0700

    feat: :sparkles: creating checkout route

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from './Home'
 export { default as Shop } from './Shop'
 export { default as Authentication } from './Authentication'
+export { default as Checkout } from './Checkout'

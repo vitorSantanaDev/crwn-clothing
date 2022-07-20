020a6a29c8ac9af50d26ed178be6bf05ae8ed3f9
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-07-19 17:09:43 -0700

    feat: :art: created the component CategoriesPreview

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,30 +1,9 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import { Fragment, useContext } from 'react'
-
-import { CategoriesContext } from 'contexts'
-
-import { IProduct } from 'interfaces'
-import { Container, ProductCard } from 'components'
-
-import * as S from './styles'
+import { Container, CategoriesPreview } from 'components'
 
 export default function Shop() {
-  const { categories } = useContext(CategoriesContext)
-
   return (
     <Container>
-      {categories &&
-        Object.keys(categories).map((category) => (
-          <Fragment key={category}>
-            <S.CategoryTitle>{category.toLocaleUpperCase()}</S.CategoryTitle>
-            <S.ShopWrapper>
-              {/* @ts-ignore */}
-              {categories[category].map((product: IProduct) => (
-                <ProductCard key={product.id} {...product} />
-              ))}
-            </S.ShopWrapper>
-          </Fragment>
-        ))}
+      <CategoriesPreview />
     </Container>
   )
 }

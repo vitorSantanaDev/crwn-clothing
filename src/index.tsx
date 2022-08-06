3d913770c06d12551c22d24990030c14260ee6b0
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-05 18:02:16 -0700

    feat: :sparkles: adding redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import { store } from 'store/store'
+import { store, persistor } from 'store/store'
 
 import App from './App'
 
@@ -16,9 +17,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyle />
-      <App />
-      <ToastContainer />
+      <PersistGate persistor={persistor}>
+        <GlobalStyle />
+        <App />
+        <ToastContainer />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,22 @@
 import { compose, createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
 
 import { rootReducer } from './root.reducer'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middlewares = [logger]
 
 const composedEnhancers = compose(applyMiddleware(...middlewares))
 
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store)

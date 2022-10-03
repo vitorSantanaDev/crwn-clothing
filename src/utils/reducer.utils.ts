348ef1df96eea06542e7894dc94717215e0171d7
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-10-03 19:41:55 -0300

    refactor: :recycle: improving util reducer

--- src/utils/reducer.utils.ts
+++ src/utils/reducer.utils.ts
@@ -1,5 +1,47 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const createAction = (type: string, payload?: any) => ({
-  type,
-  payload
-})
+import { AnyAction } from 'redux'
+
+export type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>['type']
+  match: (action: AnyAction) => action is ReturnType<AC>
+}
+
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+  actionCreator: AC
+): Matchable<AC>
+
+export function withMatcher<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  AC extends (...args: any[]) => AnyAction & { type: string }
+>(actionCreator: AC): Matchable<AC>
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type
+    }
+  })
+}
+
+export type ActionWithPayload<T, P> = {
+  type: T
+  payload: P
+}
+
+export type Action<T> = { type: T }
+
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>
+
+export function createAction<T extends string>(
+  type: T,
+  payload: void
+): Action<T>
+
+export function createAction<T extends string, P>(type: T, payload: P) {
+  return { type, payload }
+}
